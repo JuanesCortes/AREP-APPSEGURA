c26919367113af3dd917726652636d2f79155b03
unknown <juan.cortes-p@labinfo.is.escuelaing.edu.co>
2022-10-19 16:31:23 -0500

    hello

--- src/main/java/edu/eci/arep/appsegura/Appsegura.java
+++ src/main/java/edu/eci/arep/appsegura/Appsegura.java
@@ -8,20 +8,10 @@ import static spark.Spark.staticFiles;
  */
 public class Appsegura {
 
-    public static void main(String... args){
-          port(getPort());
-          //staticFiles.location("/webapp");
-          get("/app",(req,res)  -> {
-        	res.type("application/json");
-        	
-            return "Esta es la cadena ====> "+req.queryParams("name");
-        });
+    public static void main(String[] args){
+        
+          get("/hello", (req, res) -> "Hello World");
     }
 
-    private static int getPort() {
-        if (System.getenv("PORT") != null) {
-            return Integer.parseInt(System.getenv("PORT"));
-        }
-        return 4567;
-    }
+    
 }
